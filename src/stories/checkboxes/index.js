10ea48054bda8d9eab2db031597719794c7d5493
Josh Pospisil <josh.pospisil@gmail.com>
2018-09-25 17:00:15 -0500

    Added addon actions to check box component

--- src/stories/checkboxes/index.js
+++ src/stories/checkboxes/index.js
@@ -1,15 +1,34 @@
 import { storiesOf } from '@storybook/vue';
 import { CheckBox } from '../../components';
+import { action } from '@storybook/addon-actions';
 
 storiesOf('General|CheckBoxes', module)
 .add('Default', () => ({
   components: { CheckBox },
+  data () {
+    return {
+      cb1: false,
+      cb2: true,
+      cb3: false
+    }
+  },
+  watch: {
+    cb1 () {
+      const { cb1 } = this;
+      action('Checkbox 1 changed')(cb1);
+    },
+    cb2 () {
+      const { cb2 } = this;
+      action('Checkbox 2 changed')(cb2);
+    },
+    cb3 () {
+      const { cb3 } = this;
+      action('Checkbox 3 changed')(cb3);
+    }
+  },
   template: `<div>
-                <check-box :checked="false" color="#e64a19"></check-box>
-                <check-box :checked="true" color="#e64a19"></check-box>
-                <check-box :checked="false" :size=1.5 class="ml-3" color="#5e35b1"></check-box>
-                <check-box :checked="true" :size=1.5 color="#5e35b1"></check-box>
-                <check-box :checked="false" :size=2 class="ml-3" color="#607d8b"></check-box>
-                <check-box :checked="true" :size=2 color="#607d8b"></check-box>
+                <check-box :checked="cb1" @click="cb1 = !cb1" color="#e64a19"></check-box>
+                <check-box :checked="cb2" @click="cb2 = !cb2" :size=1.5 class="ml-3" color="#5e35b1"></check-box>
+                <check-box :checked="cb3" @click="cb3 = !cb3" :size=2 class="ml-3" color="#607d8b"></check-box>
               </div>`
 }));
