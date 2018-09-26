1e199e77cf4690ecaeefd60ac93acf1c30eb85fc
Josh Pospisil <josh.pospisil@gmail.com>
2018-09-26 10:03:03 -0500

    Added action logger to display available icons

--- src/stories/icons/index.js
+++ src/stories/icons/index.js
@@ -1,18 +1,40 @@
 import { storiesOf } from '@storybook/vue';
 import { Icon } from '../../components';
+import { action } from '@storybook/addon-actions';
 import * as regular from '@fortawesome/free-regular-svg-icons';
+import * as solid from '@fortawesome/free-solid-svg-icons';
 
+// Function to filter icon sets
+function iconsFilter (icons) {
+  const ignored = ['far','fas'];
+  const filter = (key) => key.startsWith('fa') && !ignored.includes(key);
+  return Object.keys(icons).filter(key => filter(key));
+}
+
+// Icon sets
+const available_icons = {
+  regular: iconsFilter(regular),
+  solid: iconsFilter(solid)
+};
+
+// Elements common to all stories
 const common = {
   components: { Icon },
   addons: {
-    notes: { text: Object.keys(regular).filter(key => key.startsWith('fa') && key !== 'far').join(' | ') }
+    notes: {
+      text: JSON.stringify(available_icons, null, 2)
+    }
   },
   propsDescription: {
     type: 'regular | solid',
     name: 'see notes below'
+  },
+  mounted () {
+    action('Available Icons')(available_icons);
   }
 };
 
+// Create the stories
 storiesOf('General|Icons', module)
 .add('Regular', () => ({
   ...common,
