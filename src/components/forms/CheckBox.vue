20e40d627b5a3ffc6f1341ac57311b91773f1ab7
jlpospisil <josh.pospisil@gmail.com>
2019-01-11 14:45:09 -0600

    Linted component

--- src/components/forms/CheckBox.vue
+++ src/components/forms/CheckBox.vue
@@ -1,8 +1,23 @@
 <template>
-    <span class="text-info" @click="$emit('click')">
-        <icon :color="color" type="regular" name="fa-check-square" :size="size" v-if="checked"></icon>
-        <icon :color="color" type="regular" name="fa-square" :size="size" v-else></icon>
-    </span>
+  <span
+    class="text-info"
+    @click="$emit('click')"
+  >
+    <icon
+      v-if="checked"
+      :color="color"
+      type="regular"
+      name="fa-check-square"
+      :size="size"
+    />
+    <icon
+      v-else
+      :color="color"
+      type="regular"
+      name="fa-square"
+      :size="size"
+    />
+  </span>
 </template>
 
 <script>
@@ -10,12 +25,12 @@ import Icon from '../generic/Icon.vue';
 
 export default {
   components: {
-    Icon
+    Icon,
   },
   props: {
     checked: { type: Boolean, required: true },
     size: { type: Number, required: false, default: 1.25 },
-    color: { type: String, required: false }
-  }
-}
+    color: { type: String, required: false },
+  },
+};
 </script>
