724fa8c276c0e7d8b075c8ab624eb8e7feae45fc
Josh Pospisil <josh.pospisil@gmail.com>
2019-01-12 15:50:38 -0600

    Moved storybook container component

--- src/stories/ui/side-nav.js
+++ src/stories/ui/side-nav.js
@@ -1,8 +1,7 @@
 import { storiesOf } from '@storybook/vue';
 /* eslint-disable-next-line import/no-unresolved */
 import { SideNav } from '@cdpjs/vue-components';
-import StorybookContainer from '@cdpjs/vue-components/StorybookContainer.vue';
-// import StorybookContainer from '../StorybookContainer.vue';
+import StorybookContainer from '../../StorybookContainer.vue';
 
 storiesOf('UI|SideNav', module)
   .add('Default', () => ({
