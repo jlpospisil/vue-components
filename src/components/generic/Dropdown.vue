ae077351764dff4ba1a760dfd3d6cf75d7b1dae4
Josh Pospisil <josh.pospisil@gmail.com>
2019-01-13 14:56:58 -0600

    Started adding dropdown components

---
+++ src/components/generic/Dropdown.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="dropdown-menu">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
+
+<script>
+export default {
+  name: 'Dropdown',
+};
+</script>
---
+++ src/components/generic/DropdownItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <a class="dropdown-item" :href="url" @click="$emit('click')">
+    <slot />
+  </a>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
+
+<script>
+export default {
+  name: 'DropdownItem',
+  props: {
+    url: { type: String, default: '#' },
+  },
+};
+</script>
