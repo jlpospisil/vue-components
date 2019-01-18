1a8e00f5f584969fb7f345854aa8fc00984b02a5
Josh Pospisil <josh.pospisil@gmail.com>
2019-01-18 12:51:17 -0600

    Added some basic styling for the multiselect component to match colors of other components

--- src/components/forms/SelectList.vue
+++ src/components/forms/SelectList.vue
@@ -1,5 +1,34 @@
 <style lang="scss" scoped>
+@import '../../scss/app';
 @import "~vue-multiselect/dist/vue-multiselect.min.css";
+
+$multiselect-highlight-color: map-get($theme-colors, primary);
+$multiselect-tag-text-color: #ffffff;
+
+.multiselect {
+  .multiselect__tag {
+    background-color: $multiselect-highlight-color;
+    color: $multiselect-tag-text-color;
+
+    .multiselect__tag-icon {
+      &:after {
+        color: darken($multiselect-tag-text-color, 15%);
+      }
+
+      &:hover {
+        background-color: darken($multiselect-highlight-color, 15%);
+
+        &:after {
+          color: darken($multiselect-tag-text-color, 25%);
+        }
+      }
+    }
+  }
+
+  .multiselect__option--highlight {
+    background-color: $multiselect-highlight-color;
+  }
+}
 </style>
 
 <script>
@@ -15,7 +44,7 @@ export default {
     options: { type: Array, default: () => [] },
     multiple: { type: Boolean, default: false },
     searchable: { type: Boolean, default: false },
-    'close-on-select': { type: Boolean, default: true },
+    closeOnSelect: { type: Boolean, default: true },
     selectLabel: { type: String, default: null },
     selectGroupLabel: { type: String, default: null },
     deselectLabel: { type: String, default: null },
