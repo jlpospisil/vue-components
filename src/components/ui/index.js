053e8daf4fe2dfcea371ef087d8b16474dfb5473
Josh Pospisil <josh.pospisil@gmail.com>
2019-01-18 18:03:05 -0600

    Added application content component

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,4 +1,5 @@
 export { default as Application } from './Application.vue';
+export { default as ApplicationContent } from './ApplicationContent.vue';
 export { default as SideNav } from './SideNav.vue';
 export { default as SideNavGroup } from './SideNavGroup.vue';
 export { default as SideNavGroupHeader } from './SideNavGroupHeader.vue';
