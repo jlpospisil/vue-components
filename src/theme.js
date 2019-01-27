d9ad7635292363c33372a2744ddd463cb5e467be
Josh Pospisil <josh.pospisil@gmail.com>
2019-01-27 15:42:19 -0600

    Removed unneeded code and files left over from moving theme back to scss

--- src/theme.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-
-const scssThemeFile = path.join(__dirname, 'scss/_theme.scss');
-
-// TODO: parse scssThemeFile into JSON here
-const theme = {};
-
-module.exports = theme;
