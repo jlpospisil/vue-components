78e5feb01724f74188a91a89403be4cb1b15ec9c
jlpospisil <josh.pospisil@gmail.com>
2019-02-01 15:42:37 -0600

    Reverted accidental update that was exporting new split pane component that is incomplete

--- src/components/general/index.js
+++ src/components/general/index.js
@@ -3,4 +3,4 @@ export { default as Dropdown } from './Dropdown.vue';
 export { default as DropdownItem } from './DropdownItem.vue';
 export { default as Icon } from './Icon.vue';
 export { default as Modal } from './Modal.vue';
-export { default as SplitPane } from './SplitPane2.vue';
+export { default as SplitPane } from './SplitPane.vue';
