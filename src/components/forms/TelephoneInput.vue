fc15ccb0a6e9d5de0de66495bfcd4896ef68db6f
Josh Pospisil <josh.pospisil@gmail.com>
2019-02-09 15:26:29 -0600

    Started adding telephone input

--- src/components/forms/TelephoneInput.vue
+++ src/components/forms/TelephoneInput.vue
@@ -5,8 +5,9 @@
     </input-label>
 
     <text-input-input
-      v-bind="inputProps"
       v-model="inputValue"
+      v-bind="inputProps"
+      :min-length="forceUpdateInputValue ? 0 : 1"
       class="form-control"
       type="tel"
       @focus="$emit('focus', $event)"
@@ -45,6 +46,9 @@ export default {
   data() {
     return {
       rawValue: null,
+      // Toggle this to force input value to update.  // TODO: find a better way
+      // to force value to update when the getter is unchanged after formatting in setter
+      forceUpdateInputValue: true,
     };
   },
   computed: {
@@ -54,20 +58,41 @@ export default {
     },
     inputValue: {
       get() {
-        const { rawValue } = this;
+        let { rawValue: v } = this;
+        v = v ? v.replace(/\D/g, '') : '';
 
-        // TODO: format the value
-        console.log({ formattedValue: rawValue });
+        if (v.length > 0 && v.substr(0, 1) !== '1') {
+          v = `1${v}`;
+        }
 
-        // TODO: why doesn't this get relayed to the input value everytime?
+        const { length } = v;
 
-        return rawValue;
+        // TODO: allow user to use backspace to delete numbers
+
+        switch (true) {
+            case (length >= 11):
+              return `+${v.substr(0, 1)} (${v.substr(1, 3)}) ${v.substr(4, 3)}-${v.substr(7, 4)}`;
+            case (length > 6):
+              return `+${v.substr(0, 1)} (${v.substr(1, 3)}) ${v.substr(4, 3)}-${v.substr(7)}`;
+            case (length > 3):
+              return `+${v.substr(0, 1)} (${v.substr(1, 3)}) ${v.substr(4)}`;
+            case (length > 1):
+              return `+${v.substr(0, 1)} (${v.substr(1)}`;
+            default:
+              return v;
+        }
       },
-      set(newValue) {
-        const rawValue = newValue.replace(/\D/g, '');
+      set(rawValue) {
+        // Update the value
         this.rawValue = rawValue;
-        this.$emit('input', rawValue);
-        console.log({ newValue, rawValue });
+
+        // Force the visible input value to be updated
+        this.forceUpdateInputValue = !this.forceUpdateInputValue;
+
+        // Emit the change
+        let { inputValue } = this;
+        inputValue = inputValue ? inputValue.replace(/\D/g, '') : null;
+        this.$emit('input', inputValue);
       },
     },
   },
